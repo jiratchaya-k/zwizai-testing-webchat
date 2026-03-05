5c80e9ad1e91125bacb096ec27e56be848d4fcaf
Jiratchaya K. <jiratchaya98x@gmail.com>
2026-03-05 18:27:31 +0700

    feat: 🎸 create header component

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div className="bg-primary w-full p-4 text-white">
+            <span className="text-lg font-bold">SWIZ.AI Testing Webchat</span>{' '}
+            By Jiratchaya Kongmuang
+        </div>
+    )
+}
+
+export default Header
