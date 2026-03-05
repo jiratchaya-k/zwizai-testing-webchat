7462bd193f39bb1d798759601f92ee5aa007ac1b
Jiratchaya K. <jiratchaya98x@gmail.com>
2026-03-06 03:35:39 +0700

    feat: 🎸 create broadcast api

---
+++ services/broadcast.service.ts
@@ -0,0 +1,20 @@
+import { TextMessage } from '@line/bot-sdk'
+
+import sendBroadcast from '@libs/line/broadcast.client'
+
+const BoardcastService = {
+    async sendMessage(textMessage: string) {
+        try {
+            const message: TextMessage = {
+                type: 'text',
+                text: textMessage,
+            }
+            console.log('Sending broadcast message:', message)
+            await sendBroadcast([message])
+        } catch (error) {
+            console.error('Error sending broadcast message:', error)
+        }
+    },
+}
+
+export default BoardcastService
