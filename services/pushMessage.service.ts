05966ab440b455f8077ddec1bba656d0013c2181
Jiratchaya K. <jiratchaya98x@gmail.com>
2026-03-06 10:01:26 +0700

    feat: 🎸 set chat list when send message from web

--- services/pushMessage.service.ts
+++ services/pushMessage.service.ts
@@ -1,5 +1,7 @@
 import { TextMessage } from '@line/bot-sdk'
 
+import { chatStore, IChat } from '@stores/chat.store'
+
 import { pushMessage } from '@libs/line/message.client'
 
 const PushMessageService = {
@@ -8,6 +10,39 @@ const PushMessageService = {
             const message: TextMessage = {
                 type: 'text',
                 text: textMessage,
+            }
+
+            const chatHistory = chatStore.getState().chatList || []
+            const existingChat = chatHistory.find(
+                (chat) => chat.sender.uid === userId,
+            )
+
+            if (existingChat) {
+                existingChat.messageList.push({
+                    text: textMessage,
+                    timestamp: Date.now(),
+                    type: 'sent',
+                })
+                chatStore.setState({
+                    chatList: [...chatHistory],
+                })
+            } else {
+                const newChat: IChat = {
+                    sender: {
+                        uid: userId,
+                        displayName: 'LINE OA',
+                    },
+                    messageList: [
+                        {
+                            text: textMessage,
+                            timestamp: Date.now(),
+                            type: 'sent',
+                        },
+                    ],
+                }
+                chatStore.setState({
+                    chatList: [...chatHistory, newChat],
+                })
             }
 
             await pushMessage(userId, [message])
