61d2e784d116963df863ab8ad2416a37cd51c0de
Jiratchaya K. <jiratchaya98x@gmail.com>
2026-03-06 11:12:10 +0700

    feat: 🎸 convert message timestamp

--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -2,6 +2,7 @@
 import { useState } from 'react'
 
 import { useMutation, useQuery } from '@tanstack/react-query'
+import dayjs from 'dayjs'
 
 import { chatStore, IChat } from '@stores/chat.store'
 
@@ -48,8 +49,8 @@ const Chat = () => {
     }
 
     return (
-        <div className="grid w-full grid-rows-[1fr_200px]">
-            <div className="grid grid-rows-[50px_1fr]">
+        <div className="w-full">
+            <div className="grid h-full w-full grid-rows-[50px_calc(100dvh-310px)_200px]">
                 <div className="bg-secondary border-primary flex items-center border-b-2 px-4 font-bold text-white">
                     {chatHistory
                         ? chatHistory.sender.displayName
@@ -61,28 +62,30 @@ const Chat = () => {
                             <Bubble
                                 key={index}
                                 message={message.text}
-                                time={message.timestamp.toString()}
+                                time={dayjs(message.timestamp).format('HH:mm')}
                                 type={message.type}
                             />
                         )
                     })}
                 </div>
-            </div>
-            <div className="mx-4 flex flex-col items-end justify-center gap-2">
-                <textarea
-                    className="border-primary w-full resize-none rounded-2xl border-2 p-4 focus:outline-none"
-                    placeholder="Write Message..."
-                    wrap="soft"
-                    rows={3}
-                    value={messageToSend}
-                    onChange={(e) => setMessageToSend(e.target.value)}
-                />
-                <button
-                    className="bg-primary cursor-pointer rounded-2xl px-10 py-3 text-white"
-                    onClick={handleSendMessage}
-                >
-                    Send
-                </button>
+                {activeChat && (
+                    <div className="border-primary flex flex-col items-end justify-center gap-2 border-t-2 px-4">
+                        <textarea
+                            className="border-primary w-full resize-none rounded-2xl border-2 p-4 shadow-md focus:outline-none"
+                            placeholder="Write Message..."
+                            wrap="soft"
+                            rows={3}
+                            value={messageToSend}
+                            onChange={(e) => setMessageToSend(e.target.value)}
+                        />
+                        <button
+                            className="bg-primary cursor-pointer rounded-2xl px-10 py-3 text-white shadow-md"
+                            onClick={handleSendMessage}
+                        >
+                            Send
+                        </button>
+                    </div>
+                )}
             </div>
         </div>
     )
