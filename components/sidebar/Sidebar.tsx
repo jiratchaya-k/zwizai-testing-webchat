287d82003962b113a300f5f3f0078e27bbd0b022
Jiratchaya K. <jiratchaya98x@gmail.com>
2026-03-08 00:58:50 +0700

    feat: 🎸 change polling history time

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -22,7 +22,7 @@ const Sidebar: FC = () => {
             const data = await res.json()
             return data
         },
-        refetchInterval: 3000,
+        refetchInterval: 2000,
     })
 
     return (
